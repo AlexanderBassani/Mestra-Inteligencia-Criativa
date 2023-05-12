d289138167e9ee61993047e4a04bdbcd32e1a965
Alexander Bassani Amaral <alexanderba09@gmail.com>
2023-05-12 15:14:36 -0300

    section 2

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+
+
+// //adjust page scale
+// const html = document.getElementsByTagName('html')
+// function scalePage() {
+//     const pageWidth = html[0].clientWidth
+//     if(pageWidth <= 900){
+//         const scale = ((pageWidth*100)/900)/100
+//         console.log(scale)
+//         html[0].setAttribute("style", `transform: scale(${scale}); transform-origin: 0 0;`);
+//     }
+    
+// }
+
+// window.addEventListener("resize", (event) => {
+//     console.log(event)
+//     scalePage()
+// });
