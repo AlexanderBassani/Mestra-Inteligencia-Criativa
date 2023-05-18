c09f99b65ac863550f773808c6ef2852392556e4
Alexander Bassani Amaral <alexanderba09@gmail.com>
2023-05-18 09:46:31 -0300

    acerto js 9

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,7 @@ $(function () {
             console.log(topPage >= 0 && topPage <= ($('#a-mestra').offset().top - 30) && currentHash != '')
             console.log(topPage)
             console.log($('#a-mestra').offset().top)
-            if(topPage >= 0 && topPage <= ($('#a-mestra').offset().top - 30) && currentHash != ''){
+            if(topPage >= 0 && topPage <= ($('#a-mestra').offset().top + 30) && currentHash != ''){
                 window.history.pushState({}, "", url);
                 currentHash = ''
             }
