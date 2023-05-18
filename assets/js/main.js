3126921ed363fe4fa948b9ea84ad5453cca2006f
Alexander Bassani Amaral <alexanderba09@gmail.com>
2023-05-18 06:48:44 -0300

    acertos href

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,17 +4,26 @@ const url = 'https://mestrainteligencia.com.br/'
 $(function () {
     var currentHash = "#one"
     $(document).scroll(function () {
+        console.log($(this).offset())
         $('.hash').each(function () {
             var top = window.pageYOffset;
             var distance = top - $(this).offset().top;
             var hash = $(this).attr('href');
 
+            if(distance == 0){
+                window.history.pushState({}, "", url);
+            }
+
             if (distance < 30 && distance > -30 && currentHash != hash) {
-                alert(hash);
+                // alert(hash);
                 currentHash = hash;
                 console.log(url + '/teste')
                 window.history.pushState({}, "", url + hash);
                 
+            }
+            
+            if((window.innerHeight + Math.round(window.scrollY)) >= document.body.offsetHeight){
+                window.history.pushState({}, "", url + 'contato');
             }
         });
     });
