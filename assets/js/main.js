2ee5b47bfe53e30d965f1b90760f8e80a97b7b8a
Alexander Bassani Amaral <alexanderba09@gmail.com>
2023-05-18 06:28:33 -0300

    jquery e hash

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,21 @@
+const url = 'https://mestrainteligencia.com.br/'
+// const url = window.location.href
 
+$(function () {
+    var currentHash = "#one"
+    $(document).scroll(function () {
+        $('.hash').each(function () {
+            var top = window.pageYOffset;
+            var distance = top - $(this).offset().top;
+            var hash = $(this).attr('href');
 
-// //adjust page scale
-// const html = document.getElementsByTagName('html')
-// function scalePage() {
-//     const pageWidth = html[0].clientWidth
-//     if(pageWidth <= 900){
-//         const scale = ((pageWidth*100)/900)/100
-//         console.log(scale)
-//         html[0].setAttribute("style", `transform: scale(${scale}); transform-origin: 0 0;`);
-//     }
-    
-// }
-
-// window.addEventListener("resize", (event) => {
-//     console.log(event)
-//     scalePage()
-// });
+            if (distance < 30 && distance > -30 && currentHash != hash) {
+                alert(hash);
+                currentHash = hash;
+                console.log(url + '/teste')
+                window.history.pushState({}, "", url + hash);
+                
+            }
+        });
+    });
+});
