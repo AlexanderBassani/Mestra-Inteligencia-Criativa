b30197c13c20621ca5db9d7a36dd13b36103921e
Alexander Bassani Amaral <alexanderba09@gmail.com>
2023-05-29 17:57:33 -0300

    google tag

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,10 +40,3 @@ $(function () {
         });
     });
 });
-
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-11189522722');
